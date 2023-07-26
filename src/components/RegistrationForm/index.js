baa639aa1b23efdfe51366580771969e8efb0d0d
avik067 <avikbiswas067>
2023-07-26 18:30:39 +0000

    not final

--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -2,12 +2,14 @@ import {Component} from 'react'
 import './index.css'
 
 class RegistrationForm extends Component {
-  state = {fName: '', lName: '', fblur: false, lblur: false}
+  state = {fName: '', lName: '', fblur: false, lblur: false, submit: true}
 
   firstBlur = () => {
     const {fName} = this.state
     if (fName === '') {
       this.setState({fblur: true})
+    } else {
+      this.setState({fblur: false})
     }
   }
 
@@ -15,6 +17,8 @@ class RegistrationForm extends Component {
     const {lName} = this.state
     if (lName === '') {
       this.setState({lblur: true})
+    } else {
+      this.setState({lblur: false})
     }
   }
 
@@ -30,42 +34,97 @@ class RegistrationForm extends Component {
 
   onSubmitFrom = event => {
     event.preventDefault()
+    const {fName, lName} = this.state
+    if (fName === '' || lName === '') {
+      console.log('Not Submitted')
+    } else {
+      console.log('Submitted')
+      this.setState({submit: false})
+    }
   }
 
-  render() {
-    const {fName, lName} = this.state
+  submitAgain = () => {
+    this.setState({
+      fName: '',
+      lName: '',
+      fblur: false,
+      lblur: false,
+      submit: true,
+    })
+  }
+
+  renderForm = () => {
+    const {fName, lName, fblur, lblur} = this.state
+
     return (
       <div className=" main">
         <form onSubmit={this.onSubmitFrom}>
           <h1>Registration</h1>
-
           <div className="col input-section">
             <label htmlFor="fname">FIRST NAME</label>
             <input
+              placeholder="First name"
               type="text"
               id="fname"
               value={fName}
               onBlur={this.firstBlur}
               onChange={this.firstChnage}
             />
-            <p className="alert">Require</p>
+            {fblur ? <p className="alert">Required</p> : ''}
             <br />
             <label htmlFor="lname">LAST NAME</label>
             <input
+              placeholder="Last name"
               type="text"
               id="lname"
               value={lName}
               onBlur={this.secondBlur}
               onChange={this.secondChnage}
             />
-            <p className="alert">Require</p>
-          </div>
-          <div className="but-container">
-            <button type="submit">Submit</button>
+            {lblur ? <p className="alert">Required</p> : ''}
+            <br />
+            <div className="but-container">
+              <button type="submit">Submit</button>
+            </div>
           </div>
         </form>
       </div>
     )
+  }
+
+  renderResult = () => {
+    console.log('ksbf')
+    return (
+      <div className="main">
+        <form onSubmit={this.onSubmitFrom}>
+          <h1>Registration</h1>
+          <div className="col input-section">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+              alt="success"
+            />
+            <p>Submitted Successfully</p>
+            <div className="but-container">
+              <button type="button" onClick={this.submitAgain}>
+                Submit Another Response
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+  render() {
+    const {submit} = this.state
+
+    let result
+    if (submit) {
+      result = this.renderForm()
+    } else {
+      result = this.renderResult()
+    }
+    return result
   }
 }
 
